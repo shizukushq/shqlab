9249f30fc16cd6006332a97437484cb6dd3241e1
shizuku <shizukushq@gmail.com>
2025-08-29 19:28:16 +0300

    improve

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,24 +1,10 @@
 import './index.css';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import App from './App.tsx';
 
 createRoot(document.body).render(
 	<StrictMode>
 		<App />
-		<span className="pointer-events-none fixed bottom-3 left-1/2 -translate-x-1/2 text-xs text-neutral-500 dark:text-neutral-400">
-			Made by{' '}
-			<a
-				href="https://github.com/shizukushq"
-				target="_blank"
-				draggable="false"
-				className="text-neutral-900 dark:text-neutral-100"
-			>
-				shizuku
-			</a>
-		</span>
-		<span className="pointer-events-none fixed top-4 left-4 font-medium">
-			shq-lab
-		</span>
 	</StrictMode>,
 );
