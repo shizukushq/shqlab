a27a0419dce35e5bd49d5acd438df2b9c95efc64
shizuku <shizukushq@gmail.com>
2025-08-29 11:18:51 +0300

    migrate to tweakpane and update deps

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,7 +1,37 @@
+import { useLayoutEffect } from 'react';
+import { useTheme } from './hooks/useTheme';
+import { Pane } from 'tweakpane';
+
 export default function App() {
+	const [isDark, setDark] = useTheme();
+
+	useLayoutEffect(() => {
+		const params = { dark: isDark, grid: true };
+		const pane = new Pane({ title: 'Settings' });
+		pane
+			.addBinding(params, 'dark', { label: 'Dark mode:' })
+			.on('change', (e) => setDark(e.value));
+		pane
+			.addBinding(params, 'grid', { label: 'Grid:' })
+			.on('change', (e) =>
+				e.value
+					? document.documentElement.classList.add('bg-grid')
+					: document.documentElement.classList.remove('bg-grid'),
+			);
+
+		// PARAMS
+		const f = pane.addFolder({
+			title: 'Params',
+			expanded: true,
+		});
+
+		return () => pane.dispose();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<main className="relative flex h-full w-full items-center justify-center">
-			{/*  */}
+			{/**/}
 		</main>
 	);
 }
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,30 +1,24 @@
 import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Sprite from './components/Sprite.tsx';
 import App from './App.tsx';
-import Config from './components/Config.tsx';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.body).render(
 	<StrictMode>
-		<Sprite />
 		<App />
-		<Config />
-		<div className="pointer-events-none fixed bottom-0 z-100 flex w-full items-center justify-center py-3 text-xs">
-			<span className="text-neutral-500 dark:text-neutral-400">
-				Made by{' '}
-				<a
-					href="https://github.com/shizukushq"
-					target="_blank"
-					draggable="false"
-					className="text-neutral-900 dark:text-neutral-100"
-				>
-					shizuku
-				</a>
-			</span>
-		</div>
+		<span className="pointer-events-none fixed bottom-3 left-1/2 -translate-x-1/2 text-xs text-neutral-500 dark:text-neutral-400">
+			Made by{' '}
+			<a
+				href="https://github.com/shizukushq"
+				target="_blank"
+				draggable="false"
+				className="text-neutral-900 dark:text-neutral-100"
+			>
+				shizuku
+			</a>
+		</span>
 		<span className="pointer-events-none fixed top-4 left-4 font-medium">
-			shq lab
+			shq-lab
 		</span>
 	</StrictMode>,
 );
